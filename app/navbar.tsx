819593812260766c8b95aa89abd40ac5369e0380
tapan0p <mahatatapan2000@gmail.com>
2024-01-07 21:38:27 +0530

    upload

---
+++ app/navbar.tsx
@@ -0,0 +1,11 @@
+"use client"
+const Navbar=()=>{
+    return(
+        <div className="flex h-10">
+            <div className="h-10 w-10" style={{backgroundImage:"url(/horse.png)"}}>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
